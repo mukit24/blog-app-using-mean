d68e544bfb45b5050e7a31450a89b49d6c77df92
mukit24 <mukithasanpranto4@gmail.com>
2023-07-30 16:50:20 +0600

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,7 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -13,26 +13,32 @@ export class PostCreateComponent implements OnInit{
   @Output() postCreated = new EventEmitter<Post>();
   postId: string;
   post: Post;
+  form: FormGroup;
   mode = 'create';
   constructor(private postService: PostsService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, { validators: [Validators.required]}),
+      content: new FormControl(null, { validators: [Validators.required]})
+    })
     this.route.params.subscribe( params => {
       this.postId = params['postId'];
       if(this.postId) {
         this.mode = 'edit';
         this.postService.getPost(this.postId).subscribe(data => {
           this.post = data.post;
+          this.form.setValue({ title: this.post.title, content: this.post.content });
         })
       }
     })
   }
 
-  onSavePost(form: NgForm) {
+  onSavePost() {
     const newPost: Post = {
       id: null,
-      title: form.value.title,
-      content: form.value.content,
+      title: this.form.value.title,
+      content: this.form.value.content,
     }
 
     if (this.mode === 'create') {
@@ -48,6 +54,6 @@ export class PostCreateComponent implements OnInit{
       })
     }
     
-    form.reset();
+    this.form.reset();
   }
 }
